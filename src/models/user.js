2a79d52a56759f6639fd5de5ee9d311a235fd703
AlyAbdulloh <uwongsabar12@gmail.com>
2024-02-17 19:00:39 +0700

    feat

--- src/models/user.js
+++ src/models/user.js
@@ -1,31 +1,35 @@
-const DataTypes  = require('sequelize');
-const sequelize = require('../../config/sequelize');
+const DataTypes = require("sequelize");
+const sequelize = require("../../config/sequelize");
 
-const User = sequelize.define('User', {
+const User = sequelize.define(
+  "User",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     username: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     role: {
-        type: DataTypes.STRING,
-        defaultValue: 'user',
-    }
-}, {
-    tableName: 'users',
-});
+      type: DataTypes.STRING,
+      defaultValue: "user",
+    },
+  },
+  {
+    tableName: "users",
+  }
+);
 
 module.exports = User;
